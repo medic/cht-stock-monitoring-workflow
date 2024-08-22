5bb347c388930059f5d8c588d8c337710d02bce3
Foromo Ernest Teoro <foromo@medic.org>
2024-08-22 11:57:44 +0100

    Add jest, mock and test example

---
+++ test/example.spec.js
@@ -0,0 +1,6 @@
+
+describe('JEST unit test example', () => {
+  it('', async() => {
+    expect(2+2).toBe(4);
+  });
+});
---
+++ test/mocks/mocks.js
@@ -0,0 +1,16 @@
+
+const ItemRowMock = {
+  type: '', // Row type
+  name: '', // Row name
+  label: '', // Item description
+  required: '', // If Row item is required or not
+  relevant: '', // If Row item is related to another (display condition)
+  appearance: '', // How should the Row item be displayed
+  calculation: '', // If the item is calculated
+  default: '', // The default value of the item if there is any
+  constraint: '' // Constraints if there is any
+};
+
+module.exports = {
+  ItemRowMock
+};
