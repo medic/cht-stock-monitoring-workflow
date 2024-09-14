5ffe615e24d599e877daa4b41e22080a947c5889
Foromo Ernest Teoro <foromo@medic.org>
2024-09-14 13:25:31 +0100

    fix: feedback correction and finalise unit test

--- test/mocks/mocks.js
+++ test/mocks/mocks.js
@@ -1,35 +1,51 @@
-const stockCountScenario = [
-  'init', 
-  '2_levels', 
-  'c62_chw', 
-  'chw', 
-  'c52_supervisor', 
-  'supervisor', 
-  'Y', 
-  'stock_count', 
-  '[{contact_type: \'c62_chw\', role: \'chw\', place_type: \'c60_chw_site\' },{contact_type: \'c52_supervisor\',role: \'supervisor\',place_type: \'c50_supervision_area\'}]',
-  'action', 
-  'end_of_week', 
-  ['Stock count', 'Stock count'],
-  'patient_assessment_under_5',
-  'Y',
-  'now()',
-  'malaria',
-  ['Category', 'Categorie'],
-  ['Category', 'Categorie'],
-  'paracetamol',
-  ['Paracetamol', 'Paracetamole'],
-  'Y',
-  ['Box of 8', 'Boite de 8'],
-  8,
-  ['Tablet', 'Comprimes'],
-  20,
-  15,
-  15,
-  'by_user',
-  0
-];
-
 module.exports = {
-  stockCountScenario
+  stockCountScenario: {
+    initScenario: [
+      'init', 
+      '2_levels', 
+      'c62_chw', 
+      'chw', 
+      'c52_supervisor', 
+      'supervisor', 
+      'Y', 
+      'stock_count', 
+      '[{contact_type: \'c62_chw\', role: \'chw\', place_type: \'c60_chw_site\' },{contact_type: \'c52_supervisor\',role: \'supervisor\',place_type: \'c50_supervision_area\'}]',
+      'action', 
+      'end_of_week', 
+      ['Stock count', 'Stock count'],
+      'patient_assessment_under_5',
+      'Y',
+      'now()',
+      'malaria',
+      ['Category', 'Categorie'],
+      ['Category', 'Categorie'],
+      'paracetamol',
+      ['Paracetamol', 'Paracetamole'],
+      'Y',
+      ['Box of 8', 'Boite de 8'],
+      8,
+      ['Tablet', 'Comprimes'],
+      20,
+      15,
+      15,
+      'by_user',
+      0
+    ],
+    productCategoryScenario: [
+      'malaria'
+    ],
+    productsScenario: [
+      'paracetamol___set',
+      'paracetamol___unit',
+      'paracetamol',
+      'paracetamol___count'
+    ]
+
+  }
 };
+
+
+
+
+
+
