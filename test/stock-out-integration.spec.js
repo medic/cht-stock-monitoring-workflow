69f6cb96ec8368f616db4044b4d84a3635270fad
Foromo Ernest Teoro <foromo@medic.org>
2024-10-22 01:40:32 +0100

    fix: review feedback and added unit test stock out

--- test/stock-out-integration.spec.js
+++ test/stock-out-integration.spec.js
@@ -1,13 +1,13 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
 const fs = require('fs-extra');
-//const { once } = require('events');
 const ExcelJS = require('exceljs');
 
 const { stockOutScenario, stockCountScenario } = require('./mocks/mocks');
 const { 
   setDirToprojectConfig,
-  revertBackToProjectHome
+  revertBackToProjectHome,
+  cleanUp
 } = require('./test-utils');
 
 
@@ -20,6 +20,7 @@ describe('Stock out', () => {
   });
 
   afterEach(async() => {
+    cleanUp(workingDir);
     revertBackToProjectHome(workingDir);
   });
 
@@ -28,15 +29,15 @@ describe('Stock out', () => {
     const workbook = new ExcelJS.Workbook();
     const childProcess = spawnSync('../../main.js',  stockOutScenario.initScenario);
 
-    if(childProcess.status === 0) {
+    if (childProcess.error) {
+      throw childProcess.error;
+    }
+    else {
       // Check that stock monitoring is initialized and stock count and stock out xform and properties files are generated
-      //const stockMonitoringConfig = path.join(processDir, 'stock-monitoring.config.json');
+      const createdAppFormFiles = ['stock_count.properties.json', 'stock_count.xlsx'];
 
-      const formFiles = fs.readdirSync(path.join(processDir, 'forms', 'app'));
-      for(const formFile of formFiles){
-        if(formFile.toString().includes('stock_count') || formFile.toString().includes('stock_out')){
-          expect(fs.existsSync(path.join(processDir, 'forms', 'app', formFile))).toBe(true);
-        }
+      for(const createdAppFormFile of createdAppFormFiles){
+        expect(fs.existsSync(path.join(processDir, 'forms', 'app', createdAppFormFile))).toBe(true);
       }
 
       expect(fs.existsSync(path.join(processDir, 'stock-monitoring.config.json'))).toBe(true);
@@ -74,12 +75,13 @@ describe('Stock out', () => {
 
       // Add stock out feature test
       const stockOutChildProcess = spawnSync('../../main.js', stockOutScenario.addStockOutFeatureScenario);
-      if(stockOutChildProcess.status === 0){
-        const stockOutFormFiles = fs.readdirSync(path.join(processDir, 'forms', 'app'));
-        for(const formFile of stockOutFormFiles){
-          if(formFile.toString().includes('stock_out')){
-            expect(fs.existsSync(path.join(processDir, 'forms', 'app', formFile))).toBe(true);
-          }
+      if (stockOutChildProcess.error) {
+        throw stockOutChildProcess.error;
+      }
+      else {
+        const createdAppFormFiles = ['stock_out.properties.json', 'stock_out.xlsx'];
+        for(const createdAppFormFile of createdAppFormFiles){
+          expect(fs.existsSync(path.join(processDir, 'forms', 'app', createdAppFormFile))).toBe(true);
         }
         
         // Check that the products are available in stock out xform
@@ -98,46 +100,7 @@ describe('Stock out', () => {
 
         expect(stockOutProductsList.length).toBe(stockOutCellProductsList.length);
         expect(stockOutProductsList.entries).toStrictEqual(stockOutCellProductsList.entries);
-
       }
-      
-      const stockOutFormFiles = fs.readdirSync(path.join(processDir, 'forms', 'app'));
-
-      for(const formFile of stockOutFormFiles){
-        if(formFile.toString().includes('stock_out') || formFile.toString().includes('stock_count')){
-          expect(fs.unlinkSync(path.join(processDir, 'forms', 'app', formFile))).toBe(undefined);
-        }
-      }
-
-      // Removing the stock monitoring init file and stock count file
-      const stockMonitoringInitPath = path.join(processDir, 'stock-monitoring.config.json');
-      //const stockMonitoringInitFile = fs.stat
-      fs.stat(stockMonitoringInitPath, (error) => {
-        if (error) {
-          //console.log(error);
-        }
-        else {
-          expect(fs.unlinkSync(stockMonitoringInitPath)).toBe(undefined);
-          //console.log(stats);
-        }
-      });
-
-      const translationFiles = fs.readdirSync(path.join(processDir, 'translations'));
-      for(const translationFile of translationFiles){
-        
-        const messageFileContent = fs.readFileSync(path.join(processDir, 'translations', translationFile), {encoding: 'utf-8'});
-        expect(messageFileContent).not.toBe('');
-        const newMessageContent = messageFileContent.split('\n').map(message => {
-          if(!message.toString().includes('cht-stock-monitoring-workflow') && message.toString()!==''){
-            return `${message.toString()}\n`;
-          }
-        });
-
-        expect(newMessageContent.includes('cht-stock-monitoring-workflow')).toBe(false);
-        fs.truncate(path.join(processDir, 'translations', translationFile), 0, function () {});
-        fs.writeFile(path.join(processDir, 'translations', translationFile),newMessageContent.toString().replaceAll(',', ''));
-      }
-      
     }
 
   });
--- test/test-utils.js
+++ test/test-utils.js
@@ -1,5 +1,6 @@
 const process = require('process');
 const path = require('path');
+const fs = require('fs-extra');
 
 const currentWorkingDirectory = () =>{
   return process.cwd();
@@ -14,10 +15,43 @@ const revertBackToProjectHome = (projectHome) =>{
   process.chdir(projectHome);
 };
 
+const cleanUp = (workingDir) => {
+  const processDir = path.join(workingDir,'test/project-config/');
+  const stockOutFormFiles = ['stock_out.xlsx', 'stock_count.xlsx', 'stock_count.properties.json', 'stock_out.properties.json'];
+  for(const formFile of stockOutFormFiles){
+    fs.unlinkSync(path.join(processDir, 'forms', 'app', formFile));
+  }
+
+  // Removing the stock monitoring init file and stock count file
+  const stockMonitoringInitPath = path.join(processDir, 'stock-monitoring.config.json');
+  fs.stat(stockMonitoringInitPath, (error) => {
+    if (!error) {
+      fs.unlinkSync(stockMonitoringInitPath);
+    }
+  });
+
+  const translationFiles = fs.readdirSync(path.join(processDir, 'translations'));
+  for(const translationFile of translationFiles){
+        
+    const messageFileContent = fs.readFileSync(path.join(processDir, 'translations', translationFile), {encoding: 'utf-8'});
+    if(messageFileContent !== ''){
+      const newMessageContent = messageFileContent.split('\n').map(message => {
+        if(!message.toString().includes('cht-stock-monitoring-workflow.stock_count') && message.toString()!==''){
+          return `${message.toString()}\n`;
+        }
+      });
+
+      fs.truncate(path.join(processDir, 'translations', translationFile), 0, function () {});
+      fs.writeFile(path.join(processDir, 'translations', translationFile),newMessageContent.toString().replaceAll(',', ''));
+    }
+  }
+};
+
 module.exports = {
   setDirToprojectConfig,
   currentWorkingDirectory,
-  revertBackToProjectHome
+  revertBackToProjectHome,
+  cleanUp,
 };
 
 
